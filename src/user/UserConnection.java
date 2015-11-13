9e5f2d9e466b2be64787810477c68255a4e1f2e5
David <slayne31@gmail.com>
2015-11-13 08:40:52 +0100

    communicatio  file statique

--- src/user/UserConnection.java
+++ src/user/UserConnection.java
@@ -3,15 +3,17 @@ package user;
 /**
  * Created by Yoan on 07/11/2015.
  */
-import message.MsgListener;
+import message.*;
 
 import javax.jms.*;
+import javax.jms.Message;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 /**
  * Created by david on 06/11/2015.
@@ -55,10 +57,12 @@ public class UserConnection {
             connection.start();
 
             System.out.println("Waiting for messages...");
-            System.out.println("Press [return] to quit");
+            System.out.println("Press [return] to send messages to topic");
 
             waiter = new BufferedReader(new InputStreamReader(System.in));
             waiter.readLine();
+            UserConnection.testTopic();
+
         } catch (IOException exception) {
             exception.printStackTrace();
         } catch (JMSException exception) {
@@ -84,6 +88,76 @@ public class UserConnection {
                 }
             }
         }
+    }
+
+    public static void  testTopic(){
+        Context context = null;
+        ConnectionFactory factory = null;
+        Connection connection = null;
+        String factoryName = "ConnectionFactory";
+        String destName = "twitter";
+        Destination dest = null;
+        Session session = null;
+        MessageProducer sender = null;
+        String text = "Message ";
+
+
+
+        try {
+            // create the JNDI initial context.
+            context = new InitialContext();
+
+            // look up the ConnectionFactory
+            factory = (ConnectionFactory) context.lookup(factoryName);
+
+            // look up the Destination
+            dest = (Destination) context.lookup(destName);
+
+            // create the connection
+            connection = factory.createConnection();
+
+            // create the session
+            session = connection.createSession(
+                    false, Session.AUTO_ACKNOWLEDGE);
+
+            // create the sender
+            sender = session.createProducer(dest);
+
+            // start the connection, to enable message sends
+            connection.start();
+
+            ArrayList<message.Message> l = message.Message.testM();
+            for (message.Message m : l) {
+                TextMessage message = session.createTextMessage();
+                message.setText(m.getContent());
+                sender.send(message);
+                System.out.println("Sent: " + message.getText());
+            }
+
+        } catch (JMSException exception) {
+            exception.printStackTrace();
+        } catch (NamingException exception) {
+            exception.printStackTrace();
+        } finally {
+            // close the context
+            if (context != null) {
+                try {
+                    context.close();
+                } catch (NamingException exception) {
+                    exception.printStackTrace();
+                }
+            }
+
+            // close the connection
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (JMSException exception) {
+                    exception.printStackTrace();
+                }
+            }
+        }
+
     }
 
 
